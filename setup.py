edd485d7e103d50f41b2715f33fda5242be90879
Zebedee Mason <zeb@cyclerouter.co.uk>
2019-10-10 23:34:45 +0100

    Update for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ packages.extend(SubDirecs(os.path.join(root, "cornichon"), "cornichon/"))
 
 setuptools.setup(
     name="cornichon",
-    version="0.9.2",
+    version="0.9.3",
     author="Zeb Mason",
     author_email="consulting@cyclerouter.co.uk",
     maintainer="Zeb Mason",
